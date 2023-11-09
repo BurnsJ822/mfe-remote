fe4ee2e3929fa5d8cc9875fabd27cb86b72c1ff8
Joseph Burns <iburnsx822@gmail.com>
2023-11-09 14:03:44 -0500

    ToDo Component Call

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Counter from './Counter';
+import TodoList from './ToDo';
 
 function App() {
   return (
     <>
       <Counter />
       <span>Hello from remote</span>;
+      <TodoList />
     </>
   );
 }
